6aecb73e2783be522e5bffef08e191dcf1ac2d4c
Domenica Katherine Amores Cedeño <domenicaamores2006@gmail.com>
2026-02-12 14:44:10 -0500

    Consulta a usuario de datos para presupuesto de consultoría +  template para PDF

---
+++ proyecto01.py
@@ -0,0 +1,7 @@
+from fpdf import FPDF
+#Consulamos informacion requerida para el documento
+print("-------Presupuesto de Consultoria-------")
+proyecto = input("Escriba el nombre del proyecto: ")
+horas_estimadas = input("Estime horas estimadas a la dedicacion del proyecto: ")
+valor_hora = input("Escriba el valor a cobrar por hora: ")
+fecha_entrega = input("Fecha de entrega estimada: ")
